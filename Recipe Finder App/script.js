a3eaf8fe2f3cdd6cfc39a2d2570caf4ce8223e71
Imama Ansari <ansariimama@yahoo.com>
2025-11-16 23:37:04 +0500

    Some JS Code

---
+++ Recipe Finder App/script.js
@@ -0,0 +1,8 @@
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#searchBtn")
+const result = document.querySelector("#result")
+console.log(searchBtn);
+
+searchBtn.addEventListener("click", () => {
+
+})
