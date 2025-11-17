691bdf143fcc41bb351d1737c54145910d9b41c1
Imama Ansari <ansariimama@yahoo.com>
2025-11-17 22:42:02 +0500

    Add Some JS Code

--- Recipe Finder App/script.js
+++ Recipe Finder App/script.js
@@ -1,8 +1,28 @@
+// GET THE VALUES
 const searchInput = document.querySelector("#searchInput");
-const searchBtn = document.querySelector("#searchBtn")
-const result = document.querySelector("#result")
-console.log(searchBtn);
+const searchBtn = document.querySelector("#searchBtn");
+const result = document.querySelector("#result");
 
 searchBtn.addEventListener("click", () => {
+    const userInput = searchInput.value.trim();
+    // Check empty input
+    if (userInput === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter something!',
+        });
+        return;
+    }
 
-})
+    // Call the API function (VERY IMPORTANT)
+    fetchRecipe(userInput);
+});
+
+// Function structure (we will fill logic next)
+async function fetchRecipe(userInput) {
+
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + userInput;
+
+
+}
